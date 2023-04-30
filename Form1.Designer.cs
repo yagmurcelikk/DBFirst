deb6d5d67131dd2f166ddc60f5db647eeda89a56
Yağmur ÇELİK <121048307+yagmurcelikk@users.noreply.github.com>
2023-05-01 00:06:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace DbFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnlistele = new System.Windows.Forms.Button();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.txtStok = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Fuchsia;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(648, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnlistele
+            // 
+            this.btnlistele.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnlistele.Location = new System.Drawing.Point(475, 19);
+            this.btnlistele.Name = "btnlistele";
+            this.btnlistele.Size = new System.Drawing.Size(106, 45);
+            this.btnlistele.TabIndex = 1;
+            this.btnlistele.Text = "Listele";
+            this.btnlistele.UseVisualStyleBackColor = true;
+            this.btnlistele.Click += new System.EventHandler(this.btnlistele_Click);
+            // 
+            // btnekle
+            // 
+            this.btnekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnekle.Location = new System.Drawing.Point(476, 99);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(105, 45);
+            this.btnekle.TabIndex = 2;
+            this.btnekle.Text = "Ekle";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // btnsil
+            // 
+            this.btnsil.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsil.Location = new System.Drawing.Point(477, 169);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(104, 45);
+            this.btnsil.TabIndex = 3;
+            this.btnsil.Text = "Sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbKategori);
+            this.groupBox1.Controls.Add(this.txtFiyat);
+            this.groupBox1.Controls.Add(this.btnlistele);
+            this.groupBox1.Controls.Add(this.btnsil);
+            this.groupBox1.Controls.Add(this.txtStok);
+            this.groupBox1.Controls.Add(this.btnekle);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(36, 200);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(598, 238);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(97, 161);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(121, 24);
+            this.cmbKategori.TabIndex = 5;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(98, 74);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 22);
+            this.txtFiyat.TabIndex = 10;
+            // 
+            // txtStok
+            // 
+            this.txtStok.Location = new System.Drawing.Point(97, 122);
+            this.txtStok.Name = "txtStok";
+            this.txtStok.Size = new System.Drawing.Size(100, 22);
+            this.txtStok.TabIndex = 9;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(98, 27);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Stok";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Categori";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fiyat";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnlistele;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.TextBox txtStok;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbKategori;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace DbFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlistele_Click(object sender, EventArgs e)
+        {
+            using (ETrade12Entities db = new ETrade12Entities())
+            {
+                var query = from a in db.Tbl_Urunler
+                            join b in db.Tbl_Categoriler on a.CategoriId equals b.Id
+                            select new { a.Id, a.Name, a.Fiyat, a.Stok, b.Aciklama };
+
+                dataGridView1.DataSource = query.ToList();
+            }
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            using (ETrade12Entities db = new ETrade12Entities())
+            {
+                Tbl_Urunler yeniUrun = new Tbl_Urunler();
+                yeniUrun.Name = txtName.Text;
+                yeniUrun.Fiyat = Convert.ToDecimal(txtFiyat.Text);
+                yeniUrun.Stok = Convert.ToInt32(txtStok.Text);
+                yeniUrun.CategoriId = Convert.ToInt32(cmbKategori.SelectedValue);
+
+                var urun = (from a in db.Tbl_Urunler
+                            join b in db.Tbl_Categoriler on a.CategoriId equals b.Id
+                            where a.Id == yeniUrun.Id
+                            select new { a.Id, a.Name, a.Fiyat, a.Stok, b.Aciklama }).FirstOrDefault();
+
+                db.Tbl_Urunler.Add(yeniUrun);
+                db.SaveChanges();
+                MessageBox.Show("Ürün başarıyla eklendi.");
+                txtName.Clear();
+                txtFiyat.Clear();
+                txtStok.Clear();
+                cmbKategori.SelectedIndex = -1;
+
+                dataGridView1.DataSource = db.Tbl_Urunler.Select(u => new { u.Id, u.Name, u.Fiyat, u.Stok, u.Tbl_Categoriler.Aciklama }).ToList();
+            }
+
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            using (ETrade12Entities db = new ETrade12Entities())
+            {
+                int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+                var silinecekUrun = db.Tbl_Urunler.Find(id);
+                db.Tbl_Urunler.Remove(silinecekUrun);
+                db.SaveChanges();
+                MessageBox.Show("Ürün başarıyla silindi.");
+                var query = from a in db.Tbl_Urunler
+                            join b in db.Tbl_Categoriler on a.CategoriId equals b.Id
+                            select new { a.Id, a.Name, a.Fiyat, a.Stok, b.Aciklama };
+                dataGridView1.DataSource = query.ToList();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (ETrade12Entities db = new ETrade12Entities())
+            {
+                cmbKategori.DataSource = db.Tbl_Categoriler.ToList();
+                cmbKategori.DisplayMember = "Aciklama";
+                cmbKategori.ValueMember = "Id";
+            }
+        }
+    }
+}
